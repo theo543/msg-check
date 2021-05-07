92c005ee962583403fade70fccaa8e149727237a
theo-3 <85434590+theo-3@users.noreply.github.com>
2021-05-07 12:42:46 +0300

    Rename variables

--- msg-check.py
+++ msg-check.py
@@ -5,14 +5,14 @@ import subprocess
 import re
 import os
 
-colorCode = r"(?<=\033\[)9(?=[0-9])(?![0-9][0-9])"  # find 90-99 ansi codes (bright colors) and match only 9
-commentLine = re.compile("^#.*\n?", re.MULTILINE)  # find comment lines
-afterSlash = r"[^\/\\]+$"  # find last item in a path
-getSlash = r"[\/\\]"  # find slash
+color_code = r"(?<=\033\[)9(?=[0-9])(?![0-9][0-9])"  # find 90-99 ansi codes (bright colors) and match only 9
+comment_line = re.compile("^#.*\n?", re.MULTILINE)  # find comment lines
+after_slash = r"[^\/\\]+$"  # find last item in a path
+get_slash = r"[\/\\]"  # find slash
 
 f = open(sys.argv[1], "r")
-msg = re.sub(commentLine, "", f.read())  # remove comments
-path = re.sub(afterSlash, "bad-commit-message-blocker", os.path.realpath(sys.argv[0]))  # supports symlinks
+msg = re.sub(comment_line, "", f.read())  # remove comments
+path = re.sub(after_slash, "bad-commit-message-blocker", os.path.realpath(sys.argv[0]))  # supports symlinks
 
 if not os.path.isdir(path):
     print("script folder not found, downloading...")
@@ -21,7 +21,7 @@ if not os.path.isdir(path):
         print("download failed")
         exit(0)
 
-path = path + re.search(getSlash, path).group(0) + "bad_commit_message_blocker.py"
+path = path + re.search(get_slash, path).group(0) + "bad_commit_message_blocker.py"
 
 if not os.path.exists(path):
     print("script not found, downloading...")
@@ -33,7 +33,7 @@ if not os.path.exists(path):
 proc = subprocess.run(args=["python", path, "--message", msg], capture_output=True, text=True)
 
 # replace bright color codes with normal codes - git breaks bright codes for some reason
-out = re.sub(colorCode, "3", str(proc.stdout))
+out = re.sub(color_code, "3", str(proc.stdout))
 
 print(out)
 
