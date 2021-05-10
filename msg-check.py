938ca151a23ff09aec540ff03b4b1ebee8f09f48
theo-3 <85434590+theo-3@users.noreply.github.com>
2021-05-10 09:18:17 +0300

    Improve config file validation
    All values must be present and must be a natural number.
    Invalid values will be reset.

--- msg-check.py
+++ msg-check.py
@@ -6,6 +6,10 @@ import re
 import subprocess
 import sys
 
+default_config = {
+    'arguments': {'body': '72', 'subject': '50'},
+    'rules': {str(nr): '1' for nr in range(1, 7)}
+}
 parser = configparser.ConfigParser()
 color_code = r"(?<=\033\[)9(?=[0-9])(?![0-9][0-9])"  # find 90-99 ansi codes (bright colors) and match only 9
 comment_line = re.compile("^#.*\n?", re.MULTILINE)  # find comment lines
@@ -14,19 +18,25 @@ get_slash = r"[/\\]"  # find slash
 
 try:
     parser.read("config.ini")
-    parser.options("rules")
-    parser.options("arguments")
-except (FileNotFoundError, configparser.Error, IndexError) as e:
+except (FileNotFoundError, configparser.Error) as e:
+    parser = configparser.ConfigParser()  # reset parser
     cfg = open("config.ini", "w")
-    parser = configparser.ConfigParser()
-    parser.add_section("arguments")
-    parser.add_section("rules")
-    parser['arguments']['subject'] = '50'
-    parser['arguments']['body'] = '72'
-    for i in range(1, 6):
-        parser['rules'][str(i)] = 'true'
+    parser.read_dict(default_config)
     parser.write(cfg)  # reset config file
     print("Error reading config, config reset.")
+
+# validate config
+changed = False
+for section, properties in default_config.items():
+    if not parser.has_section(section):
+        parser.add_section(section)
+    for val in properties:
+        if not(parser.has_option(section, val) and re.fullmatch("[0-9]*", parser[section][val])):
+            parser[section][val] = default_config[section][val]
+            changed = True
+if changed:
+    parser.write(open("config.ini", "w"))
+    print("Missing/invalid config values reset.")
 
 f = open(sys.argv[1], "r")
 msg = re.sub(comment_line, "", f.read())  # remove comments
